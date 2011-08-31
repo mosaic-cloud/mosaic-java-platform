4e74aae7cf4feee3d6a324d036e4fe5ee3a281cb
Georgiana Macariu <georgiana@ieat.ro>
2011-08-31 14:55:18 +0300

    ...

---
+++ mosaic-mvn/mosaic-cloudlet/scripts/run-component.bash
@@ -0,0 +1,21 @@
+#!/dev/null
+
+if ! test "${#}" -ge 1 ; then
+	echo "[ee] invalid arguments; aborting!" >&2
+	exit 1
+fi
+
+_identifier="${1:-000000009bd607dfc15147519d8cb1daf64e932d}"
+shift 1
+
+_jar="${_java_jars:-${_workbench}/target}/${_package_jar_name}"
+
+_java_args+=(
+		-jar "${_jar}"
+		"${@}"
+)
+
+mkdir -p "/tmp/mosaic/components/${_identifier}"
+cd "/tmp/mosaic/components/${_identifier}"
+
+exec env "${_java_env[@]}" "${_java}" "${_java_args[@]}"
