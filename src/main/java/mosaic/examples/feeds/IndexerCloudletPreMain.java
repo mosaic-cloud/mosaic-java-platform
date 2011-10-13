81744f9877b2c84eac10c15cd5e880dcdea8cbe6
Georgiana Macariu <georgiana@ieat.ro>
2011-10-13 14:11:57 +0300

    Added main.

---
+++ src/main/java/mosaic/examples/feeds/IndexerCloudletPreMain.java
@@ -0,0 +1,27 @@
+package mosaic.examples.feeds;
+
+import mosaic.cloudlet.CloudletContainerPreMain.CloudletContainerParameters;
+
+import com.google.common.base.Preconditions;
+
+import eu.mosaic_cloud.components.implementations.basic.BasicComponentHarnessPreMain;
+
+public class IndexerCloudletPreMain {
+
+	/**
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String[] arguments) throws Exception {
+		Preconditions.checkArgument(arguments != null);
+		Preconditions.checkArgument(arguments.length == 2,
+				"invalid arguments: <clodlet jar> <cloudlet descriptor>");
+
+		CloudletContainerParameters.classpath = null;
+		CloudletContainerParameters.configFile = "indexer-cloudlet.prop";
+		BasicComponentHarnessPreMain
+				.main(new String[] { "mosaic.cloudlet.runtime.ContainerComponentCallbacks" });
+
+	}
+
+}
