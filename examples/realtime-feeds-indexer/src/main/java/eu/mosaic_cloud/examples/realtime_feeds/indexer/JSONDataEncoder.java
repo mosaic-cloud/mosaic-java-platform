3613a3016c7c2af1d3426ac474c99f1286344c4e
Georgiana Macariu <georgiana@ieat.ro>
2012-02-22 15:06:31 +0200

    Fixed JSONEncoder exception handling.

--- examples/realtime-feeds-indexer/src/main/java/eu/mosaic_cloud/examples/realtime_feeds/indexer/JSONDataEncoder.java
+++ examples/realtime-feeds-indexer/src/main/java/eu/mosaic_cloud/examples/realtime_feeds/indexer/JSONDataEncoder.java
@@ -17,39 +17,57 @@
  * limitations under the License.
  * #L%
  */
+
 package eu.mosaic_cloud.examples.realtime_feeds.indexer;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 
 import eu.mosaic_cloud.platform.core.utils.DataEncoder;
+import eu.mosaic_cloud.platform.core.utils.EncodingException;
+
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
 public class JSONDataEncoder implements DataEncoder<JSONObject> {
 
-	@Override
-	public byte[] encode(JSONObject data) throws Exception {
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		Writer writer = new BufferedWriter(new OutputStreamWriter(baos));
-		data.write(writer);
-		writer.close();
-		return baos.toByteArray();
-	}
+    @Override
+    public byte[] encode(JSONObject data) throws EncodingException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        Writer writer = new BufferedWriter(new OutputStreamWriter(baos));
+        try {
+            data.write(writer);
+            writer.close();
+        } catch (JSONException e) {
+            throw new EncodingException("JSON object cannot be serialized", e);
+        } catch (IOException e) {
+            throw new EncodingException("JSON object cannot be serialized", e);
+        }
 
-	@Override
-	public JSONObject decode(byte[] dataBytes) throws Exception {
-		if (dataBytes.length == 0) {
-			return null;
-		}
-		BufferedReader reader = new BufferedReader(new InputStreamReader(
-				new ByteArrayInputStream(dataBytes)));
-		JSONTokener tokener = new JSONTokener(reader);
-		return new JSONObject(tokener);
-	}
+        return baos.toByteArray();
+    }
+
+    @Override
+    public JSONObject decode(byte[] dataBytes) throws EncodingException {
+        if (dataBytes.length == 0) {
+            return null;
+        }
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(
+                dataBytes)));
+        JSONTokener tokener = new JSONTokener(reader);
+        JSONObject object = null;
+        try {
+            object = new JSONObject(tokener);
+        } catch (JSONException e) {
+            throw new EncodingException("JSON object cannot be deserialized", e);
+        }
+        return object;
+    }
 }
