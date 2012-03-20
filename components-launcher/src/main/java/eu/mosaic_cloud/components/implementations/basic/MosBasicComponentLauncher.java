d92b09eccc608445352f32496cdf766747d1a047
Ciprian Dorin, Craciun <ciprian@volution.ro>
2012-03-20 19:13:00 +0200

    Update components launcher in-line with latest bootstrapping mechanism

--- components-launcher/src/main/java/eu/mosaic_cloud/components/implementations/basic/MosBasicComponentLauncher.java
+++ components-launcher/src/main/java/eu/mosaic_cloud/components/implementations/basic/MosBasicComponentLauncher.java
@@ -31,8 +31,8 @@ import java.nio.charset.Charset;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
 
-import eu.mosaic_cloud.components.core.ComponentCallbacks;
 import eu.mosaic_cloud.components.implementations.basic.BasicComponentHarnessMain.ArgumentsProvider;
 import eu.mosaic_cloud.tools.classpath_exporter.ClasspathExporter;
 import eu.mosaic_cloud.tools.exceptions.core.CaughtException;
@@ -49,9 +49,9 @@ import eu.mosaic_cloud.tools.threading.tools.Threading;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Joiner;
-import com.google.common.base.Objects;
 import com.google.common.base.Preconditions;
 import com.google.common.io.ByteStreams;
+import com.google.common.util.concurrent.Atomics;
 
 import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.LoggerContext;
@@ -66,83 +66,72 @@ public final class MosBasicComponentLauncher
 		throw (new UnsupportedOperationException ());
 	}
 	
-	public static final void main (final Class<? extends ComponentCallbacks> callbacksClass, final String[] arguments)
+	public static final void main (final String callbacksClass, final String[] arguments, final ClassLoader classLoader)
 			throws Throwable
 	{
 		Preconditions.checkNotNull (callbacksClass);
-		MosBasicComponentLauncher.main (callbacksClass.getName (), arguments, callbacksClass.getClassLoader ());
-	}
-	
-	public static final void main (final String callbacksClass, final String[] arguments)
-			throws Throwable
-	{
-		MosBasicComponentLauncher.main (callbacksClass, arguments, null);
-	}
-	
-	public static final void main (final String callbacksClass, final String[] arguments, final ClassLoader loader)
-			throws Throwable
-	{
-		Preconditions.checkNotNull (callbacksClass);
-		Preconditions.checkArgument (arguments != null);
+		Preconditions.checkNotNull (arguments);
+		Preconditions.checkNotNull (classLoader);
 		final String[] finalArguments = new String[arguments.length + 1];
 		finalArguments[0] = callbacksClass;
 		System.arraycopy (arguments, 0, finalArguments, 1, arguments.length);
-		MosBasicComponentLauncher.main (finalArguments, loader);
+		MosBasicComponentLauncher.main (finalArguments, classLoader);
 	}
 	
 	public static final void main (final String[] arguments)
 			throws Throwable
 	{
-		MosBasicComponentLauncher.main (arguments, null);
+		MosBasicComponentLauncher.main (arguments, ClassLoader.getSystemClassLoader ());
 	}
 	
-	public static final void main (final String[] arguments, final ClassLoader loader)
+	public static final void main (final String[] arguments, final ClassLoader classLoader)
 			throws Throwable
 	{
+		Preconditions.checkNotNull (arguments);
+		Preconditions.checkNotNull (classLoader);
 		BasicThreadingSecurityManager.initialize ();
 		final BaseExceptionTracer exceptions = AbortingExceptionTracer.defaultInstance;
 		final BasicThreadingContext threading = BasicThreadingContext.create (BasicComponentHarnessMain.class, exceptions, exceptions.catcher);
 		threading.initialize ();
-		MosBasicComponentLauncher.main (arguments, loader, threading, exceptions);
+		MosBasicComponentLauncher.main (arguments, classLoader, threading, exceptions);
 		threading.destroy ();
 	}
 	
-	public static final void main (final String[] arguments, final ClassLoader loader, final ThreadingContext threading, final ExceptionTracer exceptions)
+	public static final void main (final String[] arguments, final ClassLoader classLoader, final ThreadingContext threading, final ExceptionTracer exceptions)
 			throws Throwable
 	{
+		Preconditions.checkNotNull (arguments);
+		Preconditions.checkNotNull (classLoader);
 		Preconditions.checkNotNull (threading);
 		Preconditions.checkNotNull (exceptions);
 		final Logger logger = (Logger) LoggerFactory.getLogger (MosBasicComponentLauncher.class);
-		Preconditions.checkArgument ((arguments != null) && (arguments.length >= 2) && (("local".equals (arguments[1]) && (arguments.length == 5)) || ("remote".equals (arguments[1]) && (arguments.length == 6))), "invalid arguments: expected `<class> local <ip> <port> <mos-url>` or `<class> remote <ip> <port-1> <port-2> <mos-url>`");
-		final boolean[] shouldStop = new boolean[] {false};
-		{
-			Threading.registerExitCallback (threading, MosBasicComponentLauncher.class, "exit-hook-1", new Runnable () {
-				@Override
-				public final void run ()
-				{
-					shouldStop[0] = true;
-					logger.debug ("starting safety hook...");
-					Threading.createAndStartDaemonThread (threading, MosBasicComponentLauncher.class, "exiter", new Runnable () {
-						@Override
-						public final void run ()
-						{
-							Threading.sleep (AbortingExceptionTracer.defaulExitTimeout);
-							Threading.halt ();
-						}
-					});
-				}
-			});
-		}
-		final String clasz = arguments[0];
-		final URL controller;
-		final String[] createParameters;
+		Preconditions.checkArgument ((arguments != null) && (arguments.length >= 2) && (("local".equals (arguments[1]) && (arguments.length == 6)) || ("remote".equals (arguments[1]) && (arguments.length == 7))), "invalid arguments: expected `<component-class> local <local-ip> <local-port> <mos-url> <component-configuration>` or `<component-callbacks> remote <local-ip> <local-port-1> <local-port-2> <mos-url> <component-configuration>`");
+		Threading.registerExitCallback (threading, MosBasicComponentLauncher.class, "exit-hook", new Runnable () {
+			@Override
+			public final void run ()
+			{
+				logger.debug ("starting safety hook...");
+				Threading.sleep (AbortingExceptionTracer.defaulExitTimeout);
+				Threading.halt ();
+			}
+		});
+		final AtomicReference<String> componentIdentifier = Atomics.newReference (null);
+		final String componentCallbacksClassName = arguments[0];
+		final URL controllerBaseUrl;
+		final String[] controllerCreateUrlArguments;
 		final Runnable run;
 		if ("local".equals (arguments[1])) {
+			final String componentConfigurationEncoded = arguments[5];
 			final InetSocketAddress channelAddress = new InetSocketAddress (arguments[2], Integer.parseInt (arguments[3]));
-			controller = new URL (arguments[4]);
-			final String[] claszParts = clasz.split ("\\.");
-			final List<String> configuration = Arrays.asList (claszParts[claszParts.length - 1], String.format (String.format ("tcp:%s:%d", channelAddress.getAddress ().getHostAddress (), Integer.valueOf (channelAddress.getPort ()))));
-			createParameters = new String[] {String.format ("type=%s", URLEncoder.encode ("#mosaic-tests:socat", "UTF-8")), String.format ("configuration=%s", DefaultJsonCoder.defaultInstance.encodeToString (configuration)), "count=1"};
+			final String channelAddressEncoded = String.format ("tcp:%s:%d", channelAddress.getAddress ().getHostAddress (), Integer.valueOf (channelAddress.getPort ()));
+			{
+				final String[] componentCallbacksClassNameParts = componentCallbacksClassName.split ("\\.");
+				final List<String> controllerCreateConfiguration = Arrays.asList (componentCallbacksClassNameParts[componentCallbacksClassNameParts.length - 1], channelAddressEncoded);
+				final String controllerCreateTypeEncoded = URLEncoder.encode ("#mosaic-tests:socat", "UTF-8");
+				final String controllerCreateConfigurationEncoded = URLEncoder.encode (DefaultJsonCoder.defaultInstance.encodeToString (controllerCreateConfiguration), "UTF-8");
+				controllerCreateUrlArguments = new String[] {String.format ("type=%s", controllerCreateTypeEncoded), String.format ("configuration=%s", controllerCreateConfigurationEncoded), "count=1"};
+				controllerBaseUrl = new URL (arguments[4]);
+			}
 			run = new Runnable () {
 				@Override
 				public final void run ()
@@ -151,19 +140,19 @@ public final class MosBasicComponentLauncher
 						@Override
 						public final String getCallbacksClass ()
 						{
-							return (clasz);
+							return (componentCallbacksClassName);
 						}
 						
 						@Override
 						public final List<String> getCallbacksOptions ()
 						{
-							return (null);
+							return (Arrays.asList (componentConfigurationEncoded));
 						}
 						
 						@Override
 						public final String getChannelEndpoint ()
 						{
-							return (String.format ("%s:%d", channelAddress.getAddress ().getHostAddress (), Integer.valueOf (channelAddress.getPort ())));
+							return (channelAddressEncoded);
 						}
 						
 						@Override
@@ -175,7 +164,7 @@ public final class MosBasicComponentLauncher
 						@Override
 						public final String getIdentifier ()
 						{
-							return (null);
+							return (componentIdentifier.get ());
 						}
 						
 						@Override
@@ -185,20 +174,28 @@ public final class MosBasicComponentLauncher
 						}
 					};
 					try {
-						BasicComponentHarnessMain.main (argumentsProvider, loader, threading, null, exceptions);
+						BasicComponentHarnessMain.main (argumentsProvider, classLoader, threading, null, exceptions);
 					} catch (final Throwable exeption) {
 						throw (CaughtException.create (ExceptionResolution.Deferred, exeption).wrap ());
 					}
 				}
 			};
 		} else if ("remote".equals (arguments[1])) {
-			final InetSocketAddress httpAddress = new InetSocketAddress (arguments[2], Integer.parseInt (arguments[3]));
-			final InetSocketAddress logbackAddress = new InetSocketAddress (arguments[2], Integer.parseInt (arguments[4]));
-			controller = new URL (arguments[5]);
-			final List<String> configuration = Arrays.asList (clasz, String.format ("http://%s:%d/", httpAddress.getAddress ().getHostAddress (), Integer.valueOf (httpAddress.getPort ())), String.format ("%s:%d", logbackAddress.getAddress ().getHostAddress (), Integer.valueOf (logbackAddress.getPort ())));
-			createParameters = new String[] {String.format ("type=%s", URLEncoder.encode ("#mosaic-components:java-container", "UTF-8")), String.format ("configuration=%s", DefaultJsonCoder.defaultInstance.encodeToString (configuration)), "count=1"};
-			final ClasspathExporter exporter = ClasspathExporter.create (httpAddress, Objects.firstNonNull (loader, ClassLoader.getSystemClassLoader ()), exceptions);
-			final SimpleSocketServer appender = new SimpleSocketServer ((LoggerContext) LoggerFactory.getILoggerFactory (), logbackAddress.getPort ());
+			final String componentConfigurationEncoded = arguments[6];
+			final Object componentConfiguration = DefaultJsonCoder.defaultInstance.decodeFromString (componentConfigurationEncoded);
+			final InetSocketAddress exporterAddress = new InetSocketAddress (arguments[2], Integer.parseInt (arguments[3]));
+			final InetSocketAddress appenderAddress = new InetSocketAddress (arguments[2], Integer.parseInt (arguments[4]));
+			final String exporterAddressEncoded = String.format ("http://%s:%d/", exporterAddress.getAddress ().getHostAddress (), Integer.valueOf (exporterAddress.getPort ()));
+			final String appenderAddressEncoded = String.format ("%s:%d", appenderAddress.getAddress ().getHostAddress (), Integer.valueOf (appenderAddress.getPort ()));
+			{
+				final List<Object> controllerCreateConfiguration = Arrays.asList (componentCallbacksClassName, componentConfiguration, exporterAddressEncoded, appenderAddressEncoded);
+				final String controllerCreateTypeEncoded = URLEncoder.encode ("#mosaic-components:java-component-container", "UTF-8");
+				final String controllerCreateConfigurationEncoded = URLEncoder.encode (DefaultJsonCoder.defaultInstance.encodeToString (controllerCreateConfiguration), "UTF-8");
+				controllerCreateUrlArguments = new String[] {String.format ("type=%s", controllerCreateTypeEncoded), String.format ("configuration=%s", controllerCreateConfigurationEncoded), "count=1"};
+				controllerBaseUrl = new URL (arguments[5]);
+			}
+			final ClasspathExporter exporter = ClasspathExporter.create (exporterAddress, classLoader, exceptions);
+			final SimpleSocketServer appender = new SimpleSocketServer ((LoggerContext) LoggerFactory.getILoggerFactory (), appenderAddress.getPort ());
 			Threading.registerExitCallback (threading, MosBasicComponentLauncher.class, "exit-hook-2", new Runnable () {
 				@Override
 				public final void run ()
@@ -248,41 +245,40 @@ public final class MosBasicComponentLauncher
 			};
 		} else
 			throw (new IllegalStateException ());
-		final Thread main = Threading.createDaemonThread (threading, MosBasicComponentLauncher.class, "main", run);
-		final String[] identifier = new String[] {null};
-		{
-			Threading.registerExitCallback (threading, MosBasicComponentLauncher.class, "exit-hook-3", new Runnable () {
-				@Override
-				public final void run ()
-				{
-					logger.debug ("stopping component...");
-					if (identifier[0] != null)
-						try {
-							final URL stopUrl = new URL (controller, String.format ("/processes/stop?key=%s", identifier[0]));
-							stopUrl.openStream ().close ();
-						} catch (final Throwable exception) {
-							exceptions.trace (ExceptionResolution.Ignored, exception);
-							logger.error ("failed stopping component; ignoring!", exception);
-						}
-				}
-			});
-		}
+		Threading.registerExitCallback (threading, MosBasicComponentLauncher.class, "exit-hook-3", new Runnable () {
+			@Override
+			public final void run ()
+			{
+				logger.debug ("stopping component...");
+				final String identifier = componentIdentifier.get ();
+				if (identifier != null)
+					try {
+						final URL stopUrl = new URL (controllerBaseUrl, String.format ("/processes/stop?key=%s", identifier));
+						stopUrl.openStream ().close ();
+					} catch (final Throwable exception) {
+						exceptions.trace (ExceptionResolution.Ignored, exception);
+						logger.error ("failed stopping component; ignoring!", exception);
+					}
+			}
+		});
 		logger.debug ("starting main...");
+		final Thread main = Threading.createDaemonThread (threading, MosBasicComponentLauncher.class, "main", run);
 		main.start ();
 		Threading.sleep (MosBasicComponentLauncher.defaultCreateDelay);
 		logger.debug ("creating component...");
 		try {
-			final URL createUrl = new URL (controller, String.format ("/processes/create?%s", Joiner.on ("&").join (Arrays.asList (createParameters))));
-			logger.debug ("{}", createUrl.toString ());
+			final URL controllerCreateUrl = new URL (controllerBaseUrl, String.format ("/processes/create?%s", Joiner.on ("&").join (Arrays.asList (controllerCreateUrlArguments))));
+			logger.debug ("{}", controllerCreateUrl.toString ());
 			try {
-				final InputStream createStream = createUrl.openStream ();
+				final InputStream createStream = controllerCreateUrl.openStream ();
 				final String createResponse = new String (ByteStreams.toByteArray (createStream), Charset.forName ("UTF-8"));
 				try {
 					final Map<?, ?> createResponseMap = (Map<?, ?>) DefaultJsonCoder.defaultInstance.decodeFromString (createResponse);
 					Preconditions.checkState (Boolean.TRUE.equals (createResponseMap.get ("ok")));
 					Preconditions.checkState (((List<?>) createResponseMap.get ("keys")).size () > 0);
-					identifier[0] = (String) ((List<?>) createResponseMap.get ("keys")).get (0);
+					final String identifier = (String) ((List<?>) createResponseMap.get ("keys")).get (0);
 					Preconditions.checkNotNull (identifier);
+					componentIdentifier.set (identifier);
 				} catch (final Throwable exception) {
 					exceptions.trace (ExceptionResolution.Deferred, exception);
 					logger.error (String.format ("failed creating component: %s; ignoring!", createResponse), exception);
@@ -298,7 +294,7 @@ public final class MosBasicComponentLauncher
 			logger.error ("failed creating component; ignoring!", exception);
 			throw (new Error (exception));
 		}
-		logger.info ("started: {}", identifier);
+		logger.info ("started: {}", componentIdentifier.get ());
 		main.join ();
 	}
 	
