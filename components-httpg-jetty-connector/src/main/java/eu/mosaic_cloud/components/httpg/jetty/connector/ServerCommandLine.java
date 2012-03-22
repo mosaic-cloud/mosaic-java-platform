5a6def72addfe32268dc9ac7ad26aca43c99b5bc
Ciprian Dorin, Craciun <ciprian@volution.ro>
2012-03-22 08:39:01 +0200

    Minor correction

--- components-httpg-jetty-connector/src/main/java/eu/mosaic_cloud/components/httpg/jetty/connector/ServerCommandLine.java
+++ components-httpg-jetty-connector/src/main/java/eu/mosaic_cloud/components/httpg/jetty/connector/ServerCommandLine.java
@@ -24,9 +24,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.Enumeration;
-import java.util.List;
 import java.util.Properties;
 
 import org.apache.commons.cli.CommandLine;
