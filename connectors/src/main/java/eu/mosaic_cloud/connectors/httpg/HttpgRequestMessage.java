33922919fb987ee2fdbf686b4eef26ced9382051
Ciprian Dorin, Craciun <ciprian@volution.ro>
2012-05-07 18:05:33 +0300

    Add initial implementation of an HTTP-Gateway connector (a clone of `AmqpQueueConsumerConnector`)

---
+++ connectors/src/main/java/eu/mosaic_cloud/connectors/httpg/HttpgRequestMessage.java
@@ -0,0 +1,45 @@
+
+package eu.mosaic_cloud.connectors.httpg;
+
+
+import java.util.Map;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableMap;
+
+
+public final class HttpgRequestMessage<TBody extends Object>
+{
+	private HttpgRequestMessage (final String version, final String method, final String path, final ImmutableMap<String, String> headers, final TBody body, final IHttpgMessageToken token)
+	{
+		super ();
+		Preconditions.checkNotNull (version);
+		Preconditions.checkNotNull (method);
+		Preconditions.checkNotNull (path);
+		Preconditions.checkNotNull (headers);
+		Preconditions.checkNotNull (token);
+		this.version = version;
+		this.method = method;
+		this.path = path;
+		this.headers = headers;
+		this.body = body;
+		this.token = token;
+	}
+	
+	public static final <TBody extends Object> HttpgRequestMessage<TBody> create (final String version, final String method, final String path, final ImmutableMap<String, String> headers, final TBody body, final IHttpgMessageToken token)
+	{
+		return (new HttpgRequestMessage<TBody> (version, method, path, headers, body, token));
+	}
+	
+	public static final <TBody extends Object> HttpgRequestMessage<TBody> create (final String version, final String method, final String path, final Map<String, String> headers, final TBody body, final IHttpgMessageToken token)
+	{
+		return (new HttpgRequestMessage<TBody> (version, method, path, ImmutableMap.copyOf (headers), body, token));
+	}
+	
+	public final TBody body;
+	public final ImmutableMap<String, String> headers;
+	public final String method;
+	public final String path;
+	public final IHttpgMessageToken token;
+	public final String version;
+}
---
+++ connectors/src/main/java/eu/mosaic_cloud/connectors/httpg/HttpgResponseMessage.java
@@ -0,0 +1,42 @@
+
+package eu.mosaic_cloud.connectors.httpg;
+
+
+import java.util.Map;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableMap;
+
+
+public final class HttpgResponseMessage<TBody extends Object>
+{
+	private HttpgResponseMessage (final String version, final int status, final ImmutableMap<String, String> headers, final TBody body, final IHttpgMessageToken token)
+	{
+		super ();
+		Preconditions.checkNotNull (version);
+		Preconditions.checkArgument ((status >= 100) && (status <= 599));
+		Preconditions.checkNotNull (headers);
+		Preconditions.checkNotNull (token);
+		this.version = version;
+		this.status = status;
+		this.headers = headers;
+		this.body = body;
+		this.token = token;
+	}
+	
+	public static final <TBody extends Object> HttpgResponseMessage<TBody> create (final String version, final int status, final ImmutableMap<String, String> headers, final TBody body, final IHttpgMessageToken token)
+	{
+		return (new HttpgResponseMessage<TBody> (version, status, headers, body, token));
+	}
+	
+	public static final <TBody extends Object> HttpgResponseMessage<TBody> create (final String version, final int status, final Map<String, String> headers, final TBody body, final IHttpgMessageToken token)
+	{
+		return (new HttpgResponseMessage<TBody> (version, status, ImmutableMap.copyOf (headers), body, token));
+	}
+	
+	public final TBody body;
+	public final ImmutableMap<String, String> headers;
+	public final int status;
+	public final IHttpgMessageToken token;
+	public final String version;
+}
