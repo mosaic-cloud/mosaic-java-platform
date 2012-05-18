02c4d04b79bb4aa59e35ac9a99b29037785401c9
Ciprian Dorin, Craciun <ciprian@volution.ro>
2012-05-18 19:34:34 +0300

    Add `install` script (intall locally and deploy in artifactory)

---
+++ platform-parent/scripts/install.bash
@@ -0,0 +1,22 @@
+#!/dev/null
+
+if ! test "${#}" -eq 0 ; then
+	echo "[ee] invalid arguments; aborting!" >&2
+	exit 1
+fi
+
+env "${_mvn_env[@]}" "${_mvn_bin}" \
+		-f "${_mvn_pom}" \
+		--also-make \
+		"${_mvn_args[@]}" \
+		clean \
+		compile \
+		package \
+		install \
+		deploy \
+		-DskipTests=true \
+		-D_maven_pom_skip_analyze=true \
+		-D_maven_pom_skip_licenses=true \
+		-D_maven_pom_skip_formatter=true
+
+exit 0
