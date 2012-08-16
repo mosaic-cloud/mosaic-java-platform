66114f3fb8ccefe54d712ad785113d291e1d11c6
Ciprian Dorin Craciun <ciprian@volution.ro>
2012-08-16 14:13:19 +0300

    Minor updates

--- components-core/src/main/java/eu/mosaic_cloud/components/core/ComponentAcquireReply.java
+++ components-core/src/main/java/eu/mosaic_cloud/components/core/ComponentAcquireReply.java
@@ -13,8 +13,21 @@ public final class ComponentAcquireReply
 		super ();
 		Preconditions.checkNotNull (descriptor);
 		Preconditions.checkNotNull (reference);
+		this.ok = true;
 		this.descriptor = descriptor;
 		this.reference = reference;
+		this.error = null;
+	}
+	
+	private ComponentAcquireReply (final Object error, final ComponentCallReference reference)
+	{
+		super ();
+		Preconditions.checkNotNull (error);
+		Preconditions.checkNotNull (reference);
+		this.ok = false;
+		this.error = error;
+		this.reference = reference;
+		this.descriptor = null;
 	}
 	
 	public static final ComponentAcquireReply create (final ComponentResourceDescriptor descriptor, final ComponentCallReference reference)
@@ -22,6 +35,13 @@ public final class ComponentAcquireReply
 		return (new ComponentAcquireReply (descriptor, reference));
 	}
 	
+	public static final ComponentAcquireReply create (final Object error, final ComponentCallReference reference)
+	{
+		return (new ComponentAcquireReply (error, reference));
+	}
+	
 	public final ComponentResourceDescriptor descriptor;
+	public final Object error;
+	public final boolean ok;
 	public final ComponentCallReference reference;
 }
